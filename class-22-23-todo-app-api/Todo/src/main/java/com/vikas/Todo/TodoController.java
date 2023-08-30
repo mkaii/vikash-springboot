c417a129e4c45a17ba0de7c43bf2b6035859fcf5
GhoshM <mainakgh1@gmail.com>
2023-08-31 00:08:50 +0530

    todo-app-continue-delete many-update-done-undone

--- class-22-23-todo-app-api/Todo/src/main/java/com/vikas/Todo/TodoController.java
+++ class-22-23-todo-app-api/Todo/src/main/java/com/vikas/Todo/TodoController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -124,6 +125,56 @@ public class TodoController {
     }
 
 
+    //mark a task as done
+
+    @PutMapping("todo/done/{id}")
+    String markTodoDone(@PathVariable Integer id)
+    {
+        for(Todo todo : todoList)
+        {
+
+            if(todo.getId().equals(id))
+            {
+                if(!todo.isDone())
+                {
+                    todo.setDone(true);
+                    return "todo done";
+                }
+                else {
+                    return "todo already marked done";
+                }
+
+            }
+        }
+
+        return "Todo not found";
+    }
+
+    @PutMapping("todo/undone/{id}")
+    String markTodoUnDone(@PathVariable Integer id)
+    {
+        for(Todo todo : todoList)
+        {
+            if(todo.getId().equals(id))
+            {
+
+                if(todo.isDone())
+                {
+                    todo.setDone(false);
+                    return "todo marked unDone";
+                }
+                else
+                {
+                    return "todo already marked un-done";
+                }
+
+            }
+        }
+
+        return "Todo not found";
+    }
+
+
 
     //delete
 
@@ -141,6 +192,63 @@ public class TodoController {
         }
 
         return "todo not found";
+    }
+
+    //delete multiple todos
+
+    @DeleteMapping("todos")
+    String removeTodos(@RequestBody List<Integer> idList)
+    {
+
+        int counter = 0;
+        for(int i=0;i<todoList.size();i++)
+        {
+            Todo currentTodoFromExistingList = todoList.get(i);
+            for(int j=0;j<idList.size();j++)
+            {
+                if(currentTodoFromExistingList.getId().equals(idList.get(j)))
+                {
+                    todoList.remove(currentTodoFromExistingList);
+                    counter++;
+                    break;
+                }
+            }
+        }
+
+        return counter  + " todos were removed";
+
+       /* int counter = 0;
+        for(Todo todo : todoList)
+        {
+
+            for(Integer id : idList)
+            {
+                if(todo.getId().equals(id))
+                {
+                    todoList.remove(todo);
+                    counter++;
+                    break;
+                }
+            }
+        }
+
+        return counter  + " todos were removed";*/
+
+
+      /*  Iterator<Todo> iterator = todoList.iterator();
+        int counter = 0;
+
+        while (iterator.hasNext()) {
+            Todo todo = iterator.next();
+            if (idList.contains(todo.getId())) {
+                iterator.remove(); // Safe removal using the iterator
+                counter++;
+            }
+        }
+
+        return counter + " todos were removed";*/
+
+
     }
 
 
